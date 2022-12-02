899cfd6a3a555e59276a55182c878f0fd5a9c1e2
pavanprabhu83 <pavanprabhu@hotmail.com>
2022-12-02 05:48:08 +0530

    udpated jenkinsfile

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.32.0"
+      version = "~> 3.0"
     }
   }
+}
+
+provider "aws" {
+  region = "ap-south-1"
 }
